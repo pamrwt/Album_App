22008675eaffe648aafb79f22b933d6f7876c5c9
User <pramilarwt30@gmail.com>
2020-02-13 23:32:50 +0530

    first commit11

--- app/src/main/java/no/pamir/pamirnewproject/MainActivity.java
+++ app/src/main/java/no/pamir/pamirnewproject/MainActivity.java
@@ -16,34 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//new changesb
 
-        JSONObject j1=new JSONObject();
-        JSONObject j2=new JSONObject();
-        JSONArray ja1 =new JSONArray();
-        JSONArray ja2 =new JSONArray();
-
-        for (int i = 1; i<= 2; i++) {
-            try {
-                JSONObject cust = new JSONObject();
-
-                cust.put("userName","user"+ i);
-                j2.put("albumName","album"+ i);
-//                j2.put("albumName","album"+String.valueOf(i));
-//                ja2.put(j2);
-//            cust.put("name","customer"+i);
-//            cust.put("url","url"+i);
-
-
-                ja2.put(j2);
-                cust.put("albumList",ja2);
-                ja1.put(cust);
-            ja1.put(ja2);
-            j1.put("Allusers ",ja1);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        }
-        Log.e("json array ",j1.toString());
-
     }
 }
