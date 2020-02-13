1b59b5ec52b1da80d6aeabc4b643ea7d090a761a
User <pramilarwt30@gmail.com>
2020-02-13 21:22:17 +0530

    first commit11

--- app/src/main/java/no/pamir/pamirnewproject/MainActivity.java
+++ app/src/main/java/no/pamir/pamirnewproject/MainActivity.java
@@ -3,6 +3,11 @@ package no.pamir.pamirnewproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//new changesb
+
+        JSONObject j1=new JSONObject();
+        JSONObject j2=new JSONObject();
+        JSONArray ja1 =new JSONArray();
+        JSONArray ja2 =new JSONArray();
+
+        for (int i = 1; i<= 2; i++) {
+            try {
+                JSONObject cust = new JSONObject();
+
+                cust.put("userName","user"+ i);
+                j2.put("albumName","album"+ i);
+//                j2.put("albumName","album"+String.valueOf(i));
+//                ja2.put(j2);
+//            cust.put("name","customer"+i);
+//            cust.put("url","url"+i);
+
+
+                ja2.put(j2);
+                cust.put("albumList",ja2);
+                ja1.put(cust);
+            ja1.put(ja2);
+            j1.put("Allusers ",ja1);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        }
+        Log.e("json array ",j1.toString());
+
     }
 }
